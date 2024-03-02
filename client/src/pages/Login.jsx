5e20701edaf0c8e713985c1011cbf00e511221db
Aajugiya Ravi <aajugiyaravi@gmail.com>
2024-03-02 18:47:00 +0530

    initial

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,51 +1,73 @@
-import {Heading} from "../components/ui/Heading.jsx";
-import {InfoHead} from "../components/ui/InfoHead.jsx";
-import {Input} from "../components/ui/Input.jsx";
-import {Button} from "../components/ui/Button.jsx";
-import {ButtonLink} from "../components/ui/ButtonLink.jsx";
-import {useState} from "react";
-import {useNavigate} from "react-router-dom";
+import { Heading } from "../components/ui/Heading.jsx";
+import { InfoHead } from "../components/ui/InfoHead.jsx";
+import { Input } from "../components/ui/Input.jsx";
+import { Button } from "../components/ui/Button.jsx";
+import { ButtonLink } from "../components/ui/ButtonLink.jsx";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import axios from "axios";
+import { useSetRecoilState } from "recoil";
+import { userAtom } from "../store/atoms/user.jsx";
 
 export const Login = () => {
+  const navigate = useNavigate();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const setUser = useSetRecoilState(userAtom);
 
-    const navigate = useNavigate();
-
-    const [username, setUsername] = useState("");
-    const [password,setPassword] = useState("");
-
-    const handleLogin = async (e) => {
-        e.preventDefault();
-
-        try {
-            const response = await axios.post('http://localhost:3000/api/v1/user/login', {
-                username,
-                password
-            });
-
-            localStorage.setItem("token", `Bearer ${response.data.token}`);
-            navigate('/dashboard');
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(
+        "http://localhost:3000/api/v1/user/login",
+        {
+          username,
+          password,
         }
-        catch (e){
-            console.error(e);
-        }
+      );
+      console.log(response);
+      setUser({
+        userId: response.data.userId,
+        username: response.data.username,
+        firstName: response.data.firstName,
+        lastName: response.data.lastName,
+      });
+
+      localStorage.setItem("token", `Bearer ${response.data.token}`);
+      navigate("/dashboard");
+    } catch (e) {
+      console.error(e);
     }
+  };
 
-    return <main className='h-screen flex justify-center'>
-        <div className='flex flex-col justify-center'>
-            <form onSubmit={handleLogin}
-                className='rounded-lg border-black border w-80 text-center p-2 h-max px-4'>
-                <Heading title={"Log In"} />
-                <InfoHead label={"Log In with your Credentials here!"} />
-                <Input label={"username"} placeholder={"email"} type={"text"}
-                onChange={e => setUsername(e.target.value)}
-                />
-                <Input label={"password"} placeholder={"password"} type={"password"}
-                onChange={e => setPassword(e.target.value)}
-                />
-                <Button title={"Submit"} type={"submit"} />
-                <ButtonLink mainLink={"Don't have an account?"} text={"Sign Up"} navigator={'/signup'} />
-            </form>
-        </div>
+  return (
+    <main className="h-screen flex justify-center">
+      <div className="flex flex-col justify-center">
+        <form
+          onSubmit={handleLogin}
+          className="rounded-lg border-black border w-80 text-center p-2 h-max px-4">
+          <Heading title={"Log In"} />
+          <InfoHead label={"Log In with your Credentials here!"} />
+          <Input
+            label={"username"}
+            placeholder={"email"}
+            type={"text"}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <Input
+            label={"password"}
+            placeholder={"password"}
+            type={"password"}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <Button title={"Submit"} type={"submit"} />
+          <ButtonLink
+            mainLink={"Don't have an account?"}
+            text={"Sign Up"}
+            navigator={"/signup"}
+          />
+        </form>
+      </div>
     </main>
-}
+  );
+};
--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,171 +1,182 @@
 const jwt = require('jsonwebtoken');
-const {signUpSchema, logInSchema, updateUserSchema} = require("./validators");
-const {User, Account} = require("../models/models");
-const {secret} = require("../config");
+const { signUpSchema, logInSchema, updateUserSchema } = require('./validators');
+const { User, Account } = require('../models/models');
+const { secret } = require('../config');
 const signUp = async (req, res) => {
-    try{
-        const {success} = signUpSchema.safeParse(req.body);
+  try {
+    const { success } = signUpSchema.safeParse(req.body);
 
-        if(!success){
-            return res.status(411).json({
-                msg: "Incorrect Inputs / zod errors!"
-            })
-        }
-
-        const existingUser = await User.findOne({
-            username: req.body.username
-        });
-
-        if(existingUser){
-            return res.status(411).json({
-                msg: "User Already exists!"
-            });
-        }
-
-        const user = await User.create({
-            username: req.body.username,
-            password: req.body.password,
-            firstName: req.body.firstName,
-            lastName: req.body.lastName
-        })
-
-        const userId = user._id;
-
-        // Initializing the user Account
-        await Account.create({
-            userId,
-            balance: 1000 + Math.random() * 10000
-        });
-
-        const token = jwt.sign({
-            userId
-        }, secret);
-
-        res.json({
-            msg: "User Signed up successfully!",
-            token: token
-        })
-    } catch (e) {
-        res.status(500).json({
-            msg: "Internal Server Error!"
-        });
-        console.log(e);
+    if (!success) {
+      return res.status(411).json({
+        msg: 'Incorrect Inputs / zod errors!',
+      });
     }
-}
+
+    const existingUser = await User.findOne({
+      username: req.body.username,
+    });
+
+    if (existingUser) {
+      return res.status(411).json({
+        msg: 'User Already exists!',
+      });
+    }
+
+    const user = await User.create({
+      username: req.body.username,
+      password: req.body.password,
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+    });
+
+    const userId = user._id;
+
+    // Initializing the user Account
+    await Account.create({
+      userId,
+      balance: 1000 + Math.random() * 10000,
+    });
+
+    const token = jwt.sign(
+      {
+        userId,
+      },
+      secret
+    );
+
+    res.json({
+      msg: 'User Signed up successfully!',
+      token: token,
+    });
+  } catch (e) {
+    res.status(500).json({
+      msg: 'Internal Server Error!',
+    });
+    console.log(e);
+  }
+};
 
 const logIn = async (req, res) => {
-    try {
-        const {success} = logInSchema.safeParse(req.body);
+  try {
+    const { success } = logInSchema.safeParse(req.body);
 
-        if (!success) {
-            return res.status(411).json({
-                msg: "Incorrect inputs! / zod errors!"
-            });
-        }
-
-        const user = await User.findOne({
-            username: req.body.username,
-            password: req.body.password
-        });
-
-        if(user){
-            const token = jwt.sign({
-                userId: user._id,
-            }, secret);
-
-            return res.json({
-                token: token,
-            });
-        }
-
-        return res.status(411).json({
-            msg: "Error while logging In!"
-        })
-    }catch (e) {
-        return res.status(500).json({
-            msg: "Internal Server Error!"
-        });
-        console.log(e);
+    if (!success) {
+      return res.status(411).json({
+        msg: 'Incorrect inputs! / zod errors!',
+      });
     }
-}
+
+    const user = await User.findOne({
+      username: req.body.username,
+      password: req.body.password,
+    });
+
+    if (user) {
+      const token = jwt.sign(
+        {
+          userId: user._id,
+        },
+        secret
+      );
+
+      return res.json({
+        token: token,
+        userId: user._id,
+        username: user.username,
+        firstName: user.firstName,
+        lastName: user.lastName,
+      });
+    }
+
+    return res.status(411).json({
+      msg: 'Error while logging In!',
+    });
+  } catch (e) {
+    return res.status(500).json({
+      msg: 'Internal Server Error!',
+    });
+    console.log(e);
+  }
+};
 
 const showUserData = async (req, res) => {
-    try {
-        const userData = await User.findById(req.userId);
-        if(userData){
-            res.json({
-                userId: userData._id,
-                username: userData.username,
-                firstName: userData.firstName,
-                lastName: userData.lastName
-            });
-        }
-        else {
-            res.json("User not found !")
-        }
-    }catch (e) {
-        console.log(e);
-        res.status(500).json({
-            msg: "Internal server Error!"
-        })
+  try {
+    const userData = await User.findById(req.userId);
+    if (userData) {
+      res.json({
+        userId: userData._id,
+        username: userData.username,
+        firstName: userData.firstName,
+        lastName: userData.lastName,
+      });
+    } else {
+      res.json('User not found !');
     }
-}
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({
+      msg: 'Internal server Error!',
+    });
+  }
+};
 
 const updateUser = async (req, res) => {
-    const { success } = updateUserSchema.safeParse(req.body);
-    if(!success) {
-        res.status(411).json({
-            msg: "Error while updating the information!"
-        });
-    }
-
-    await User.updateOne({
-        _id: req.userId
-    },
-        {
-            $set: req.body
-        });
-    console.log(req.userId);
-
-    res.json({
-        msg: "User updated successfully!"
+  const { success } = updateUserSchema.safeParse(req.body);
+  if (!success) {
+    res.status(411).json({
+      msg: 'Error while updating the information!',
     });
-}
+  }
 
+  await User.updateOne(
+    {
+      _id: req.userId,
+    },
+    {
+      $set: req.body,
+    }
+  );
+  console.log(req.userId);
+
+  res.json({
+    msg: 'User updated successfully!',
+  });
+};
 
 const searchUser = async (req, res) => {
-    const filter = req.query.filter || "";
+  const filter = req.query.filter || '';
 
-    const users = await User.find({
-        $or: [{
-            firstName: {
-                "$regex": filter,
-                "$options": "i"
-            }
+  const users = await User.find({
+    $or: [
+      {
+        firstName: {
+          $regex: filter,
+          $options: 'i',
         },
-            {
-                lastName: {
-                    "$regex": filter,
-                    "$options": "i"
-                }
-            },
-            {
-                username: {
-                    "$regex": filter,
-                    "$options": "i"
-                }
-            }
-        ]
-    });
+      },
+      {
+        lastName: {
+          $regex: filter,
+          $options: 'i',
+        },
+      },
+      {
+        username: {
+          $regex: filter,
+          $options: 'i',
+        },
+      },
+    ],
+  });
 
-    res.json({
-        user: users.map(user => ({
-                username: user.username,
-                firstName: user.firstName,
-                lastName: user.lastName,
-                _id: user._id
-        }))
-    });
-}
+  res.json({
+    user: users.map((user) => ({
+      username: user.username,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      _id: user._id,
+    })),
+  });
+};
 
-module.exports = {signUp, logIn, updateUser, searchUser, showUserData};
+module.exports = { signUp, logIn, updateUser, searchUser, showUserData };
